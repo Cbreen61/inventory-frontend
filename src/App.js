89f2d6dbe088aca1f6bb0017d6c1f4d801476245
Christopher Breen <C.jbreen61@gmail.com>
2021-04-08 11:37:17 -0400

    updated manifest.js to remove error in console for missing logo image, added test fetch to make sure data was being pasted from backend.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
-import React from 'react';
+import { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
+
+  componentDidMount(){
+    fetch('http://[::1]:3000/api/v1/regions', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }
   render(){
   return (
     "hello can you see me "
